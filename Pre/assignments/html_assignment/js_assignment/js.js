01bb9389c99fa3a16635bc7990402f887b2e7c12
Cheng Yu <yucheng618@gmail.com>
2022-07-09 15:47:41 -0400

    XPS15 -8

--- Pre/assignments/html_assignment/js_assignment/js.js
+++ Pre/assignments/html_assignment/js_assignment/js.js
@@ -27,13 +27,76 @@ function palin(str) {
 console.log(palin(""));
 
 //3.Combinations
-function combi(arr) {
-    var arr2 = [];
-    for(let i = 0; i < arr.length;i++) {
-        for(let j = i+1; j < arr.length;j++ ){
-            arr2.push(arr.slice(i,j+1));
+function combi(str) {
+    var arr = [];
+    for(let i = 0; i < str.length;i++) {
+        for(let j = i+1; j < str.length;j++ ){
+            arr.push(str.slice(i,j+1));
         }
     }
-    return arr2;
+    return arr;
 }
 console.log(combi("hello"));
+
+//4.alphabetical order
+function alpha(str) {
+    var aToZ = "abcdefghijklmnopqrstuvwxyz"
+    var str2 = "";
+    str = str.toLowerCase();
+    for(let i=0; i<aToZ.length;i++) {
+        for(let j=0; j<str.length;j++) {
+            if(aToZ[i] == str[j]){
+                str2 += str[j];
+            }
+        }
+    }
+    return str2;
+}
+
+console.log(alpha("Webmaster"));
+
+//5.to uppercase
+function toUpper(str) {
+    var aToZ = "abcdefghijklmnopqrstuvwxyz"
+    var str2 = str[0].toUpperCase();
+    for(let i=1; i<str.length; i++) {
+        if(!aToZ.includes(str[i-1])) {
+            str2 += str[i].toUpperCase();
+        }
+        else {
+            str2 += str[i];
+        }
+    }
+    return str2;
+}
+console.log(toUpper("the quick brown fox"));
+
+//6.find the longest
+function longest(str) {
+    var arr = str.split(" ");
+    var max = arr[0].length;
+    var index = 0;
+    for(let i=1; i<arr.length; i++) {
+        if(max < arr[i].length) {
+            max = arr[i].length;
+            index = i;
+        }
+    }
+    return arr[index];
+}
+console.log(longest("Web Development Turorial"))
+
+//7. find all the vowels
+function vowel(str) {
+    var refer = "aeiouAEIOU";
+    var num = 0;
+    for(let i=0; i<str.length; i++) {
+        if(refer.includes(str[i])) {
+            num ++;
+        }
+    }
+    return num;
+}
+console.log(vowel("The quick brown fox"))
+
+//8.check prime or not
