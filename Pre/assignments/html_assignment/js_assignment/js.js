145c878800fec6a17e442442577f745323850767
Cheng Yu <yucheng618@gmail.com>
2022-07-10 22:40:46 -0400

    fin

--- Pre/assignments/html_assignment/js_assignment/js.js
+++ Pre/assignments/html_assignment/js_assignment/js.js
@@ -1,15 +1,14 @@
 // 1.Reverse a number
-function reverse(num)
-{
-	num = num + "";
-	var num2 = Number(num.split("").reverse().join(""));
+function reverse(num) {
+    num = num + "";
+    var num2 = Number(num.split("").reverse().join(""));
     return num2;
 }
 console.log(reverse(32243));
 
 //2.Check palindrome
 function palin(str) {
-    if(str.split("").reverse().join("") === str) {
+    if (str.split("").reverse().join("") === str) {
         return true;
     }
     return false;
@@ -19,9 +18,9 @@ console.log(palin(""));
 //3.Combinations
 function combi(str) {
     var arr = [];
-    for(let i = 0; i < str.length;i++) {
-        for(let j = i; j < str.length;j++ ){
-            arr.push(str.slice(i,j+1));
+    for (let i = 0; i < str.length; i++) {
+        for (let j = i; j < str.length; j++) {
+            arr.push(str.slice(i, j + 1));
         }
     }
     return arr;
@@ -30,12 +29,12 @@ console.log(combi("dog"));
 
 //4.alphabetical order
 function alpha(str) {
-    var aToZ = "abcdefghijklmnopqrstuvwxyz"
+    var aToZ = "abcdefghijklmnopqrstuvwxyz";
     var str2 = "";
     str = str.toLowerCase();
-    for(let i=0; i<aToZ.length;i++) {
-        for(let j=0; j<str.length;j++) {
-            if(aToZ[i] == str[j]){
+    for (let i = 0; i < aToZ.length; i++) {
+        for (let j = 0; j < str.length; j++) {
+            if (aToZ[i] == str[j]) {
                 str2 += str[j];
             }
         }
@@ -47,13 +46,12 @@ console.log(alpha("Webmaster"));
 
 //5.to uppercase
 function toUpper(str) {
-    var aToZ = "abcdefghijklmnopqrstuvwxyz"
+    var aToZ = "abcdefghijklmnopqrstuvwxyz";
     var str2 = str[0].toUpperCase();
-    for(let i=1; i<str.length; i++) {
-        if(!aToZ.includes(str[i-1])) {
+    for (let i = 1; i < str.length; i++) {
+        if (!aToZ.includes(str[i - 1])) {
             str2 += str[i].toUpperCase();
-        }
-        else {
+        } else {
             str2 += str[i];
         }
     }
@@ -66,40 +64,39 @@ function longest(str) {
     var arr = str.split(" ");
     var max = arr[0].length;
     var index = 0;
-    for(let i=1; i<arr.length; i++) {
-        if(max < arr[i].length) {
+    for (let i = 1; i < arr.length; i++) {
+        if (max < arr[i].length) {
             max = arr[i].length;
             index = i;
         }
     }
     return arr[index];
 }
-console.log(longest("Web Development Turorial"))
+console.log(longest("Web Development Turorial"));
 
 //7. find all the vowels
 function vowel(str) {
     var refer = "aeiouAEIOU";
     var num = 0;
-    for(let i=0; i<str.length; i++) {
-        if(refer.includes(str[i])) {
-            num ++;
+    for (let i = 0; i < str.length; i++) {
+        if (refer.includes(str[i])) {
+            num++;
         }
     }
     return num;
 }
-console.log(vowel("The quick brown fox"))
+console.log(vowel("The quick brown fox"));
 
 //8.check prime or not
 function prime(num) {
-    if(num < 2) {
+    if (num < 2) {
         return false;
     }
-    if(num === 2) {
+    if (num === 2) {
         return true;
-    }
-    else {
-        for(let i=2; i<num; i++) {
-            if(num % i === 0) {
+    } else {
+        for (let i = 2; i < num; i++) {
+            if (num % i === 0) {
                 return false;
             }
         }
@@ -110,16 +107,15 @@ console.log(prime(4));
 
 //9.return type
 function type(var1) {
-    return typeof(var1);
+    return typeof var1;
 }
 console.log(type("hello"));
 
 //10.n * n matirx
 function matrix(num) {
-
-    for(let i = 0; i < num; i++) {
+    for (let i = 0; i < num; i++) {
         var str = "";
-        for(let j = 0; j < num; j++) {
+        for (let j = 0; j < num; j++) {
             str += "*";
         }
         console.log(str);
@@ -130,30 +126,31 @@ matrix(4);
 //11. second lowest and greatest numbers;
 function secon(arr) {
     var result = [];
-    arr.sort((a,b)=> {return a - b} );
+    arr.sort((a, b) => {
+        return a - b;
+    });
     result.push(arr[1]);
-    result.push(arr[arr.length-2]);
+    result.push(arr[arr.length - 2]);
     return result;
 }
-console.log(secon([1,5,2,4,3,8,6]));
+console.log(secon([1, 5, 2, 4, 3, 8, 6]));
 
 //12. perfect numbers
 function perfec(num) {
-    if(num < 1) return false;
-    var arr = []
+    if (num < 1) return false;
+    var arr = [];
     var sum = 0;
-    for(let i = 1; i < num; i++) {
-        if( num % i === 0) {
+    for (let i = 1; i < num; i++) {
+        if (num % i === 0) {
             arr.push(i);
         }
     }
-    for(let j = 0; j < arr.length; j++) {
+    for (let j = 0; j < arr.length; j++) {
         sum += arr[j];
     }
-    if(sum === num) {
+    if (sum === num) {
         return true;
-    }
-    else {
+    } else {
         return false;
     }
 }
@@ -162,8 +159,8 @@ console.log(perfec(496));
 //13. find factors
 function factor(num) {
     var factors = [];
-    for(let i = 1; i <= num; i++) {
-        if(num % i === 0 ) {
+    for (let i = 1; i <= num; i++) {
+        if (num % i === 0) {
             factors.push(i);
         }
     }
@@ -174,48 +171,48 @@ console.log(factor(10));
 //14. convert an amount to coins
 function toCoins(num, arr) {
     var result = [];
-    for(let i = 0; i < arr.length; i++) {
-        for(let j = 0; j < parseInt(num/arr[i]); j++) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < parseInt(num / arr[i]); j++) {
             result.push(arr[i]);
         }
         num = num % arr[i];
     }
     return result;
 }
-console.log(toCoins(46,[25,10,5,2,1]));
+console.log(toCoins(46, [25, 10, 5, 2, 1]));
 
 //15. compute the value of bn
 function power(b, n) {
-        return Math.pow(b,n)
-    }
-console.log(power(-2.5,3));
+    return Math.pow(b, n);
+}
+console.log(power(-2.5, 3));
 
 //16. extarct unique characters from a string
 const unique = (str) => {
     var result = "";
-    for(let i = 0; i < str.length; i++) {
-        if(!result.includes(str[i])){
+    for (let i = 0; i < str.length; i++) {
+        if (!result.includes(str[i])) {
             result += str[i];
-        } 
+        }
     }
     return result;
-}
+};
 console.log(unique("thequickbrownfoxjumpsoverthelazydog"));
 
 //17. occurrences
 function occur(str) {
     for (let i = 0; i < str.length; i++) {
         let count = 0;
-        for(let j = 0; j < str.length; j++) {
-            if(str[i] == str[j] && i > j) {
+        for (let j = 0; j < str.length; j++) {
+            if (str[i] == str[j] && i > j) {
                 break;
             }
-            if(str[i] == str[j]) {
-                count ++;
+            if (str[i] == str[j]) {
+                count++;
             }
         }
-        if(count > 0) {
-            console.log( `${str[i]} : ${count}`);
+        if (count > 0) {
+            console.log(`${str[i]} : ${count}`);
         }
     }
 }
@@ -224,56 +221,181 @@ occur("hello");
 //18. binary search
 function binary(arr, val, start, end) {
     if (start > end) return false;
-    let mid = Math.floor((start + end)/2);
-    if(arr[mid] === val) {
+    let mid = Math.floor((start + end) / 2);
+    if (arr[mid] === val) {
         return true;
     }
-    if(arr[mid] > val) {
-        return binary(arr,val, start, mid-1)
-    }
-    else {
-        return binary(arr, val, mid+1, end);
+    if (arr[mid] > val) {
+        return binary(arr, val, start, mid - 1);
+    } else {
+        return binary(arr, val, mid + 1, end);
     }
 }
-console.log(binary([1,3,5,7,8,9], 6, 0, 5));
+console.log(binary([1, 3, 5, 7, 8, 9], 6, 0, 5));
 
 //19. array elements larger than a number
 function larger(arr, num) {
     let newArr = [];
-    for(let i of arr) {
-        if(i > num) {
+    for (let i of arr) {
+        if (i > num) {
             newArr.push(i);
         }
     }
     return newArr;
 }
-console.log(larger([1,2,43,32,12,34,7,9,0], 5));
+console.log(larger([1, 2, 43, 32, 12, 34, 7, 9, 0], 5));
 
 //20. random id
-function generateId(length)
-{
+function generateId(length) {
     let string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     var result = "";
-    for(var i=0; i < length; i++ ) {  
+    for (var i = 0; i < length; i++) {
         result += string[Math.floor(Math.random() * string.length)];
     }
     return result;
 }
 console.log(generateId(10));
 
-//21. all possible subset of an array
-function subset(arr, num) {
-    if(num > arr.length) {
-        return false;
+// 21. Write a JavaScript function to get all possible subset with a fixed length (for example 2) combinations in an array.
+// Sample array: [1, 2, 3] and subset length is 2
+// Expected output: [[2, 1], [3, 1], [3, 2]]
+//
+function combinations(array, n) {
+    array2 = new Array(1 << array.length)
+        .fill()
+        .map((e1, i) => array.filter((e2, j) => i & (1 << j)));
+    return array2.filter((a) => a.length == n);
+}
+
+/*
+22. Write a JavaScript function that accepts two arguments, a string and a letter and the function will count the number of occurrences of the specified letter within the string. 
+Sample arguments: 'microsoft.com', 'o' 
+Expected output: 3 
+*/
+const Count = (str, letter) => {
+    alert(str.split(letter).length - 1);
+};
+
+/*
+23. Write a JavaScript function to find the first not repeated character. 
+Sample arguments: 'abacddbec' 
+Expected output: 'e' 
+*/
+function firstNRC(string) {
+    for (var i = 0; i < string.length; i++) {
+        var c = string.charAt(i);
+        if (string.indexOf(c) == i && string.indexOf(c, i + 1) == -1) {
+            return c;
+        }
     }
+    return null;
 }
 
-//26.longest substring
-function substr(str) {
-
+/*
+24. Write a JavaScript function to apply Bubble Sort algorithm. 
+Note: According to wikipedia "Bubble sort, sometimes referred to as sinking sort, is a simple sorting algorithm that works by repeatedly stepping through the list to be sorted, comparing each pair of adjacent items and swapping them if they are in the wrong order". 
+Sample array: [12, 345, 4, 546, 122, 84, 98, 64, 9, 1, 3223, 455, 23, 234, 213]
+Expected output: [3223, 546, 455, 345, 234, 213, 122, 98, 84, 64, 23, 12, 9, 4, 1]
+*/
+function bubbleSort(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        for (var j = 0; j < arr.length - i - 1; j++) {
+            if (arr[j] < arr[j + 1]) {
+                var temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
 }
 
-//29. get the function name. 
+/*
+25. Write a JavaScript function that accept a list of country names as input and returns the longest country name as output. 
+Sample function: Longest_Country_Name(["Australia", "Germany", "United States of America"])
+Expected output: "United States of America"
+*/
+const Longest_Country_Name = ([...a]) => {
+    let array = [...a];
+    let comparsionString = "";
+    array.map((v) => {
+        if (v.length > comparsionString.length) {
+            comparsionString = v;
+        }
+    });
+    return comparsionString;
+};
+
+/*
+26. Write a JavaScript function to find longest substring in a given a string without repeating characters
+*/
+const longestSubstring = (str) => {
+    var max_length = 0,
+        Longestsubstring = "";
+    var strChar = str.split("");
+    var result = "",
+        result2 = [];
+    for (let i = 0; i < strChar.length; i++) {
+        if (strChar[i] != strChar[i - 1]) {
+            result += strChar[i];
+            result2.push(result);
+        } else {
+            result = "";
+        }
+    }
+    let compareString = "";
+    result2.map((v, i) => {
+        if (v.length > compareString.length) {
+            compareString = v;
+        }
+    });
+    return compareString;
+};
+
+/*
+27. Write a JavaScript function that returns the longest palindrome in a given string. 
+Note: According to Wikipedia "In computer science, the longest palindromic substring or longest symmetric factor problem is the problem of finding a maximum-length contiguous substring of a given string that is also a palindrome. For example, the longest palindromic substring of "bananas" is "anana". The longest palindromic substring is not guaranteed to be unique; for example, in the string "abracadabra", there is no palindromic substring with length greater than three, but there are two palindromic substrings with length three, namely, "aca" and "ada".
+In some applications it may be necessary to return all maximal palindromic substrings (that is, all substrings that are themselves palindromes and cannot be extended to larger palindromic substrings) rather than returning only one substring or returning the maximum length of a palindromic substring.
+*/
+const longestPalindrome = (str) => {
+    var max_length = 0,
+        maxPalindrome = [],
+        result = [];
+    for (var i = 0; i < str.length; i++) {
+        var substr = str.substr(i, str.length);
+        for (let x = substr.length; x > 0; x--) {
+            var substring_substring = substr.substr(0, x);
+            if (substring_substring <= 1) continue;
+            if (
+                isPalindrome(substring_substring) &&
+                substring_substring.length >= max_length
+            ) {
+                max_length = substring_substring.length;
+                maxPalindrome.push(substring_substring);
+            }
+        }
+    }
+    maxPalindrome.map((v, i) => {
+        if (v.length == max_length) {
+            result.push(v);
+        }
+    });
+    return result;
+};
+
+/*
+28. Write a JavaScript program to pass a 'JavaScript function' as parameter. 
+*/
+
+function name(name) {
+    alert(name);
+}
+
+function callFunction(func, myname) {
+    func(myname);
+}
+
+//29. get the function name.
 function tst() {
     console.log("Run");
 }
